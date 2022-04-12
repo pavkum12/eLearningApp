cadf54e7ca54577f9c5e165917b1645cd7432fa9
pavkum12 <83943233+pavkum12@users.noreply.github.com>
2022-04-12 17:31:58 +0530

    Update Course.js

--- src/components/pages/Course.js
+++ src/components/pages/Course.js
@@ -10,7 +10,7 @@ export default function Course() {
     return (
         <header>
             <nav className="navbar navbar-expand-lg navbar-light primary-gradient shadow">
-                <Link className="nav-link brand " to="/">
+                <Link className="nav-link brand " to="/home">
                     <i className="fa-solid fa-square-caret-left text-white"></i>
                 </Link>
                 <h4 className='text-white mx-auto'>Courses</h4>
