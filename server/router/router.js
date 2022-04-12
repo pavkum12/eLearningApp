851a8ced7ee6d8c152296bf21d166f5036c82c34
pavkum12 <83943233+pavkum12@users.noreply.github.com>
2022-04-12 18:16:03 +0530

    Update router.js

--- server/router/router.js
+++ server/router/router.js
@@ -2,6 +2,7 @@ const router = require('express').Router()
 const controller = require('../controller/controller');
 const auth = require('../middleware/auth')
 
+router.get('/', controller.homePage);
 router.post('/register', controller.registerUser);
 router.post('/login', controller.login);
 router.post('/adminlogin', controller.adminlogin);
