2d28410349c37fab3ab5bd70d21df34eff3206aa
pavkum12 <83943233+pavkum12@users.noreply.github.com>
2022-04-15 15:10:18 +0530

    update server

--- server/server.js
+++ server/server.js
@@ -25,14 +25,13 @@ const router = require('express').Router()
 const controller = require('./controller/controller');
 const auth = require('./middleware/auth')
 
+app.use(express.static(path.join(__dirname, "../build")))
 console.log("in Router");
 app.post('/api/register', controller.registerUser);
 app.post('/api/login', controller.login);
 app.post('/api/adminlogin', controller.adminlogin);
 app.delete('/api/delete', auth, controller.delete);
-app.get('/', (req, res) => {
-    res.sendFile('../build/')
-})
+
 
 app.listen(PORT, () => {
     console.log(`Server is running on ${PORT}`);
